7e02f51a4682dc2e46d4d17ee24efce9753dc988
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2024-12-13 18:24:27 +0200

    basicPublish minimal setup.

---
+++ src/main/kotlin/com/mesh/kabbitMq/builders/KabbitMQPublishBuilder.kt
@@ -0,0 +1,36 @@
+package com.mesh.kabbitMq.builders
+
+import com.mesh.kabbitMq.dsl.KabbitMQDslMarker
+import com.mesh.kabbitMq.util.State
+import com.mesh.kabbitMq.util.StateDelegator
+import com.rabbitmq.client.AMQP
+import com.rabbitmq.client.Channel
+
+@KabbitMQDslMarker
+class KabbitMQPublishBuilder(private val channel: Channel) {
+    var exchange: String by StateDelegator()
+    var routingKey: String by StateDelegator()
+    var message: String by StateDelegator()
+    var mandatory: Boolean by StateDelegator()
+    var immediate: Boolean by StateDelegator()
+    var basicProperties by StateDelegator(State.Initialized(AMQP.BasicProperties()))
+
+    fun build() {
+        with(StateDelegator) {
+            when {
+                initialized(::mandatory) && initialized(::immediate) -> {
+                    channel.basicPublish(exchange, routingKey, mandatory, immediate, basicProperties, message.toByteArray())
+                }
+                initialized(::immediate) -> {
+                    channel.basicPublish(exchange, routingKey, immediate, basicProperties, message.toByteArray())
+                }
+                initialized(::mandatory) -> {
+                    channel.basicPublish(exchange, routingKey, mandatory, basicProperties, message.toByteArray())
+                }
+                else -> {
+                    channel.basicPublish(exchange, routingKey, basicProperties, message.toByteArray())
+                }
+            }
+        }
+    }
+}
