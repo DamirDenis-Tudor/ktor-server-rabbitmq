d9ccb56d422d4ba2f45cd3eebdb937d94cbd2fe5
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2025-01-15 10:47:13 +0200

    Coroutine support + dedicated dispacher.

--- src/main/kotlin/io/github/damir/denis/tudor/ktor/server/rabbitmq/dsl/PluginContext.kt
+++ src/main/kotlin/io/github/damir/denis/tudor/ktor/server/rabbitmq/dsl/PluginContext.kt
@@ -3,46 +3,83 @@ package io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.Connection
 import io.github.damir.denis.tudor.ktor.server.rabbitmq.connection.ConnectionManager
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
-class PluginContext(val connectionManager: ConnectionManager) : ChannelContext(connectionManager.getChannel())
+class PluginContext(
+    override val connectionManager: ConnectionManager
+) : ChannelContext(
+    connectionManager,
+    connectionManager.getChannel()
+)
+
+internal fun PluginContext.getChannelContext(channel: Channel): ChannelContext =
+    ChannelContext(connectionManager, channel)
+
+@RabbitDslMarker
+fun PluginContext.channel(
+    block: suspend ChannelContext.() -> Unit
+): Channel {
+    with(connectionManager) {
+        return getChannel()
+            .also {
+                coroutineScope.launch(dispatcher) {
+                    getChannelContext(it).apply { block() }
+                }
+            }
+    }
+}
 
 @RabbitDslMarker
 fun PluginContext.channel(
     id: Int,
     autoClose: Boolean = false,
-    block: ChannelContext.() -> Unit
+    block: suspend ChannelContext.() -> Unit
 ): Channel {
-    return connectionManager.getChannel(id)
-        .also {
-            ChannelContext(it).apply { block() }
-            if (autoClose) connectionManager.closeChannel(id)
-        }
+    with(connectionManager) {
+        return getChannel(id)
+            .also {
+                coroutineScope.launch(dispatcher) {
+                    getChannelContext(it).apply { block() }
+                    if (autoClose) closeChannel(id)
+                }
+            }
+    }
 }
 
 @RabbitDslMarker
 fun PluginContext.connection(
     id: String,
     autoClose: Boolean = false,
-    block: ConnectionContext.() -> Unit
+    block: suspend ConnectionContext.() -> Unit
 ): Connection {
-    return connectionManager.getConnection(id)
-        .also {
-            ConnectionContext(connectionManager, it).apply { block() }
-            if (autoClose) connectionManager.closeConnection(id)
-        }
+    with(connectionManager) {
+        return getConnection(id)
+            .also {
+                coroutineScope.launch(dispatcher) {
+                    ConnectionContext(connectionManager, it).apply { block() }
+                    if (autoClose) closeConnection(id)
+                }
+            }
+    }
 }
 
 @RabbitDslMarker
 fun PluginContext.libConnection(
     id: String,
     autoClose: Boolean = false,
-    block: Connection.() -> Unit
-): Connection  {
-    return connectionManager.getConnection(id)
-        .also {
-            it.apply { block() }
-            if (autoClose) connectionManager.closeConnection(id)
-        }
+    block: suspend Connection.() -> Unit
+): Connection {
+    with(connectionManager) {
+        return getConnection(id)
+            .also {
+                coroutineScope.launch(dispatcher) {
+                    it.apply { block() }
+                    if (autoClose) closeConnection(id)
+                }
+            }
+    }
 }
