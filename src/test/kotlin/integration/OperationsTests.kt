33585c865d5e4375afebdaff674054bcb2ce826d
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2025-01-20 14:36:05 +0200

    test: minimal consumer tests added.

--- src/test/kotlin/integration/OperationsTests.kt
+++ src/test/kotlin/integration/OperationsTests.kt
@@ -1,21 +1,28 @@
+package integration
+
+import channelTest
+import connectionTest
 import io.github.damir.denis.tudor.ktor.server.rabbitmq.RabbitMQ
 import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.*
 import io.github.damir.denis.tudor.ktor.server.rabbitmq.rabbitMQ
 import kotlinx.serialization.Serializable
 import io.ktor.server.application.*
 import io.ktor.server.testing.*
+import io.ktor.util.Digest
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.RabbitMQContainer
 import org.testcontainers.utility.DockerImageName
-import kotlin.coroutines.coroutineContext
+import rabbitmqTest
+import java.lang.Thread.sleep
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class OperationsTests {
 
@@ -68,7 +75,7 @@ class OperationsTests {
                         exchange = "dlx"
                         type = "direct"
                     }
-                }.getOrThrow()
+                }
             }
         }
     }
@@ -137,7 +144,7 @@ class OperationsTests {
                     }
                 }.getOrThrow()
 
-                Thread.sleep(2_000)
+                sleep(2_000)
 
                 assertEquals(messageCount { queue = "dlq" }.getOrNull(), 10)
 
@@ -149,9 +156,126 @@ class OperationsTests {
                     }
                 }
 
-                Thread.sleep(2_000)
+                sleep(2_000)
 
                 assertEquals(messageCount { queue = "dlq" }.getOrNull(), 0)
+            }
+        }
+    }
+
+    @Test
+    fun `consumer with coroutine poll (processing is not sequentially)`() = testApplication {
+        application {
+            install(RabbitMQ) {
+                connectionAttempts = 3
+                attemptDelay = 10
+                uri = rabbitMQContainer.amqpUrl
+            }
+        }
+
+        application {
+            rabbitmqTest {
+                queueBind {
+                    queue = "demo-queue"
+                    exchange = "demo-exchange"
+                    routingKey = "demo-routing-key"
+                    queueDeclare {
+                        queue = "demo-queue"
+                    }
+                    exchangeDeclare {
+                        exchange = "demo-exchange"
+                        type = "direct"
+                    }
+                }
+            }
+
+            rabbitmqTest {
+                repeat(1_000) {
+                    basicPublish {
+                        exchange = "demo-exchange"
+                        routingKey = "demo-routing-key"
+                        message { "Hello World!" }
+                    }
+                }
+            }
+
+            rabbitmqTest {
+                val counter = AtomicInteger(0)
+                connectionTest(id = "consume") {
+                    basicConsume {
+                        autoAck = true
+                        queue = "demo-queue"
+                        dispatcher = Dispatchers.IO
+                        coroutinePollSize = 100
+                        deliverCallback<String> { tag, message ->
+                            delay(30)
+                            withContext(Dispatchers.IO.limitedParallelism(1)) {
+                                counter.incrementAndGet()
+                            }
+                        }
+                    }
+                }
+
+                sleep(2_000)
+                log.info(counter.toString())
+                assert(counter.get() == 1_000)
+            }
+        }
+    }
+
+    @Test
+    fun `consumer with default 1 coroutine (processing is sequentially)`() = testApplication {
+        application {
+            install(RabbitMQ) {
+                connectionAttempts = 3
+                attemptDelay = 10
+                uri = rabbitMQContainer.amqpUrl
+            }
+        }
+
+        application {
+            rabbitmqTest {
+                queueBind {
+                    queue = "demo1-queue"
+                    exchange = "demo1-exchange"
+                    routingKey = "demo1-routing-key"
+                    queueDeclare {
+                        queue = "demo1-queue"
+                    }
+                    exchangeDeclare {
+                        exchange = "demo1-exchange"
+                        type = "direct"
+                    }
+                }
+            }
+
+            rabbitmqTest {
+                repeat(100) {
+                    basicPublish {
+                        exchange = "demo1-exchange"
+                        routingKey = "demo1-routing-key"
+                        message { "Hello World!" }
+                    }
+                }
+            }
+
+            rabbitmqTest {
+                val counter = AtomicInteger(0)
+                connectionTest(id = "consume1") {
+                    basicConsume {
+                        autoAck = true
+                        queue = "demo1-queue"
+                        dispatcher = Dispatchers.IO
+                        deliverCallback<String> { tag, message ->
+                            delay(3)
+                            counter.incrementAndGet()
+                        }
+                    }
+                }
+
+                sleep(2_000)
+                log.info(counter.toString())
+                assert(counter.get() == 100)
             }
         }
     }
